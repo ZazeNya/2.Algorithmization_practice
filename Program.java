5a9b2363f9475ca122f23c554652d9b235c472d6
ZazeNya <115577504+ZazeNya@users.noreply.github.com>
2023-06-18 19:10:57 +0300

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int num = 3;
+        int[] array = {4, 6, 7, 65, 87, 45, 34, 32};
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % num == 0) {
+                sum += array[i];
+            }
+        }
+        System.out.println("Сумма элементов массива A[N], кратных K = " + num + ", равна " + sum);
+
+    }
+}
